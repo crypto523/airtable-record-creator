e6f250dad9d63c621e15dae33c73718512e484f5
crypto523 <jacksoncurry46@gmail.com>
2019-08-08 07:23:30 -0700

    add airtable and prettier

--- index.js
+++ index.js
@@ -1,48 +1,76 @@
-const { Toolkit } = require('actions-toolkit')
-// const token = process.env.AIRTABLE_TOKEN
+const {Toolkit} = require("actions-toolkit");
+const Airtable = require("airtable");
+const token = process.env.AIRTABLE_TOKEN;
+const base = process.env.AIRTABLE_BASE;
 
 async function createEventObject(body) {
-  let obj = {}
-  let arr1 = body.split("```")[1]
-  let arr2 = arr1.split("\r\n")
-  arr2.shift()
-  arr2.pop()
+  let obj = {};
+  let arr1 = body.split("```")[1];
+  let arr2 = arr1.split("\r\n");
+  arr2.shift();
+  arr2.pop();
 
-  let arr3 = arr2.map((a) => a.split(":"))
-  arr3.map((a) => obj[`${a[0].replace(" ", "_").toLowerCase()}`] = a[1].trim())
+  let arr3 = arr2.map(a => a.split(":"));
+  arr3.map(a => (obj[`${a[0].replace(" ", "_").toLowerCase()}`] = a[1].trim()));
 
-  return arr3.map((a) => obj[`${a[0]}`] = a[1])
+  return arr3.map(a => (obj[`${a[0]}`] = a[1]));
+}
+
+async function createAirTableRecord(obj, url) {
+  const Airtable = require("airtable");
+  const base = new Airtable({apiKey: token}).base(base);
+  const body = await createEventObject(obj);
+  let id;
+
+  base("All IRL Events").create(
+    {
+      Event: body.event_name,
+      Location: body.location,
+      "GitHub Issue": url,
+      Status: ["Under Consideration"],
+      Triage: "Under Consideration",
+      "IRL Roadmap": "Coming Soon",
+    },
+    function(err, record) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      id = record.getId();
+    },
+  );
+
+  return id;
 }
 
 // Run your GitHub Action!
 Toolkit.run(async tools => {
-  const action = tools.context.payload.action
-  const issue = tools.context.payload.issue
+  const action = tools.context.payload.action;
+  const issue = tools.context.payload.issue;
 
-  tools.log.success(action)
-  tools.log.success(issue)
+  tools.log.success(action);
+  tools.log.success(issue);
 
   if (action !== "opened") {
-    tools.exit.neutral("Just checking for recent issues")
+    tools.exit.neutral("Just checking for recent issues");
   }
 
   try {
-    const issueUrl = issue.url
-    const body = await createEventObject(issue.body)
+    const recordId = await createAirTableRecord(issue.body, issue.url);
 
-    tools.log.success(`Issue #${issue.title} opened`)
-    tools.exit.success("Action is complete")
+    tools.log.success(`Airtable record #${recordId} created`);
+    tools.exit.success("Action is complete");
   } catch (err) {
     // Log the error message
-    tools.log.error(`An error occurred while pinning the issue.`)
-    tools.log.error(err)
+    tools.log.error(`An error occurred while pinning the issue.`);
+    tools.log.error(err);
 
     // The error might have more details
-    if (err.errors) tools.log.error(err.errors)
+    if (err.errors) tools.log.error(err.errors);
 
     // Exit with a failing status
-    tools.exit.failure()
+    tools.exit.failure();
   }
 
-  tools.exit.success('We did it!')
-})
+  tools.exit.success("We did it!");
+});
